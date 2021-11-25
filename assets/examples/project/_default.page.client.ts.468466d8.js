b64bc625efb4e7bf90098c1c52faa75e3f17556d
yxonic <yxonic@users.noreply.github.com>
2021-11-25 16:30:00 +0000

    deploy: 796e14ed8e48851c7d1c9e23461e1fa637439623

--- assets/examples/project/_default.page.client.ts.468466d8.js
+++
@@ -1,1 +0,0 @@
-import{s as o}from"../../router.df132c70.js";import"../../vendor.5275522d.js";import"../../app.8816df9c.js";o({onTransitionStart(){console.log("start transition")},onTransitionEnd(){console.log("end transition")}});
