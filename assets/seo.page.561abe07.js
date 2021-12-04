fde8aa1e53409337cba1048131ef87ecc4888b0a
yxonic <yxonic@users.noreply.github.com>
2021-12-04 16:11:53 +0000

    deploy: b3a1e69f9fcbfc15b00c20df4f74c85764139de7

--- assets/seo.page.561abe07.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as s,l as t}from"./vendor.f79b5ce3.js";const a={class:"markdown-body"},c=t("h1",{id:"seo",tabindex:"-1"},"SEO",-1),n=t("p",null,[t("em",null,"TBA")],-1),r=[c,n],u={title:"SEO - VPage Docs",layout:"book"},f={setup(l,{expose:o}){return o({frontmatter:{title:"SEO - VPage Docs",layout:"book"}}),(d,i)=>(e(),s("div",a,r))}};export{f as default,u as frontmatter};
